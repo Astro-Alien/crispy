e14f5f8b35bd0f5feb4ffc1edd52a872a169e911
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2024-09-03 22:10:41 +0200

    committing teleprompter base files

---
+++ app/teleprompter/teleprompter.js
@@ -0,0 +1,13 @@
+export default class Teleprompter extends crs.classes.BindableElement {
+    get html() {
+        return import.meta.url.replace(".js", ".html");
+    }
+
+    get shadowDom() {
+        return true;
+    }
+
+    get hasStyle() {
+        return true;
+    }
+}
