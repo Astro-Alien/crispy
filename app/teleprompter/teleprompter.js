7b86598aaf1cb208c1be0f010e14fe5cbd1ec2f8
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2024-09-04 08:04:33 +0200

    committing current version of teleprompter still has bug in it

--- app/teleprompter/teleprompter.js
+++ app/teleprompter/teleprompter.js
@@ -1,4 +1,8 @@
 export default class Teleprompter extends crs.classes.BindableElement {
+    #speed = 5;
+    #scrolling;
+    #windowEventHandler = this.#windowEvent.bind(this);
+
     get html() {
         return import.meta.url.replace(".js", ".html");
     }
@@ -10,4 +14,44 @@ export default class Teleprompter extends crs.classes.BindableElement {
     get hasStyle() {
         return true;
     }
+
+    async connectedCallback() {
+        await super.connectedCallback();
+        this.addEventListener("load", this.#windowEventHandler);
+    }
+
+    async disconnectedCallback() {
+        this.#speed = null;
+        this.#scrolling = null;
+        this.removeEventListener("load", this.#windowEventHandler);
+        this.#windowEventHandler = null;
+    }
+
+    async #play(timeout) {
+        if (!this.#scrolling) {
+            this.#scrolling = setInterval(await this.#scrollingText, 50);
+        }
+    }
+
+    async #pause() {
+        clearInterval(this.#scrolling);
+        this.#scrolling = null;
+    }
+
+    async #scrollingText() {
+        this.text.style.top = `${parseFloat(this.text.style.top) - (this.#speed / 10)}%`;
+
+        if (parseFloat(text.style.top) <= -100) {
+            this.text.style.top = '100%';
+        }
+    }
+
+    async #adjustSpeed(newSpeed) {
+        this.#speed = newSpeed;
+    }
+
+   async #windowEvent(event) {
+       this.text.style.top = '100%';
+       await this.#play();
+   }
 }
