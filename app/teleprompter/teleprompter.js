1dfa269c63343e2416db5e86f629fba886ce83ca
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2024-09-04 23:44:24 +0200

    committing v1 of teleprompter
    the code requires some cleanup that I will implement next

--- app/teleprompter/teleprompter.js
+++ app/teleprompter/teleprompter.js
@@ -1,7 +1,19 @@
+const ExclusionWords = Object.freeze({
+    PROTO: "__proto__"
+});
+
 export default class Teleprompter extends crs.classes.BindableElement {
-    #speed = 5;
-    #scrolling;
-    #windowEventHandler = this.#windowEvent.bind(this);
+    #speed = 0.25;
+    #scrolling = false;
+    #textAreaHandler = this.#hideTextArea.bind(this);
+    #requestId;
+    #HotKeyActions = Object.freeze({
+        ShiftQ: async () => { await this.play() },
+        ShiftW: async () => { await this.pause() },
+        ShiftE: () => { this.textArea.setAttribute("hidden", true) },
+        ShiftR: () => { this.textArea.removeAttribute("hidden") }
+    });
+
 
     get html() {
         return import.meta.url.replace(".js", ".html");
@@ -17,41 +29,80 @@ export default class Teleprompter extends crs.classes.BindableElement {
 
     async connectedCallback() {
         await super.connectedCallback();
-        this.addEventListener("load", this.#windowEventHandler);
+        this.dataset.position = "250%";
+        globalThis.addEventListener("keydown", this.#textAreaHandler);
     }
 
     async disconnectedCallback() {
         this.#speed = null;
         this.#scrolling = null;
-        this.removeEventListener("load", this.#windowEventHandler);
-        this.#windowEventHandler = null;
+        globalThis.removeEventListener("keydown", this.#textAreaHandler);
+        this.#textAreaHandler = null;
+        this.#HotKeyActions = null;
+        cancelAnimationFrame(this.#requestId);
     }
 
-    async #play(timeout) {
-        if (!this.#scrolling) {
-            this.#scrolling = setInterval(await this.#scrollingText, 50);
+    async play() {
+        if (this.#scrolling === false) {
+            this.#scrolling = true;
+            await this.#scroll();
         }
     }
 
-    async #pause() {
-        clearInterval(this.#scrolling);
-        this.#scrolling = null;
+    async pause() {
+        this.#scrolling = false;
+        cancelAnimationFrame(this.#requestId);
+    }
+    async #populateText(text) {
+        //Todo: sanitize the input
+        this.text.innerHTML = text;
     }
 
-    async #scrollingText() {
-        this.text.style.top = `${parseFloat(this.text.style.top) - (this.#speed / 10)}%`;
+    async #hideTextArea(event) {
+        const shiftKey = event.shiftKey === true ? "Shift" : null;
 
-        if (parseFloat(text.style.top) <= -100) {
-            this.text.style.top = '100%';
+        if (event.key === "Enter" && this.textArea.value !== "") {
+            await this.#populateText(this.textArea.value);
+        }
+
+        if (shiftKey === null) return;
+
+        const combination = shiftKey + event.key;
+        event.preventDefault();
+        if (this.#HotKeyActions[combination] != null && combination !== ExclusionWords.PROTO) {
+            await this.#HotKeyActions[combination]();
         }
     }
 
-    async #adjustSpeed(newSpeed) {
-        this.#speed = newSpeed;
+    async #scroll() {
+        if (this.#scrolling === false) return;
+
+        const currentPosition = this.dataset.position;
+        const topValue = parseFloat(currentPosition);
+        const newTopValue = topValue - this.#speed;
+        await this.#setPositionProperty(newTopValue);
+
+        if (newTopValue <= -100) {
+            this.#scrolling = false;
+            return;
+        }
+
+        this.#requestId = requestAnimationFrame(async() => {
+            await this.#scroll();
+        });
     }
 
-   async #windowEvent(event) {
-       this.text.style.top = '100%';
-       await this.#play();
-   }
+    async #setPositionProperty(value) {
+        const valueInPercentage = `${value}%`;
+        this.style.setProperty("--top", valueInPercentage);
+        this.dataset.position = value;
+
+        if (value <= -100) {
+            this.dataset.position = "250%";
+        }
+    }
+
+    async updateSpeedChanged(value) {
+        this.#speed = parseFloat(value);
+    }
 }
