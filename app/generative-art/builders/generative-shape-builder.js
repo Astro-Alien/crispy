72d82da84877e5a0b546126023f4be85b9914b26
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2024-09-18 21:48:05 +0200

    Create generative-shape-builder.js

---
+++ app/generative-art/builders/generative-shape-builder.js
@@ -0,0 +1,9 @@
+/*
+ *@class generativeShapeBuilder - the following is a builder class that will be responsible for building basic shapes
+*/
+export class GenerativeShapeBuilder {
+
+    build() {
+        //temp method
+    }
+}
